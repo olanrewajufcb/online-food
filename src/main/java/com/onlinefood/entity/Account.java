11a660e76e460985343de526115d8ff4cb918b43
olanrewajufcb <49466876+olanrewajufcb@users.noreply.github.com>
2021-03-03 16:41:58 +0100

    first commit

---
+++ src/main/java/com/onlinefood/entity/Account.java
@@ -0,0 +1,37 @@
+package com.onlinefood.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table(name = "Accounts")
+public class Account implements Serializable {
+
+    private static final long serialVersionUID = -2054386655979281969L;
+
+    public static final String ROLE_MANAGER = "MANAGER";
+    public static final String ROLE_EMPLOYEE = "EMPLOYEE";
+
+    @Id
+    @Column(name = "User_Name", length = 20, nullable = false)
+    private String userName;
+
+    @Column(name = "Encryted_Password", length = 128, nullable = false)
+    private String encrytedPassword;
+
+    @Column(name = "Active", length = 1, nullable = false)
+    private boolean active;
+
+    @Column(name = "User_Role", length = 20, nullable = false)
+    private String userRole;
+
+
+
+}
